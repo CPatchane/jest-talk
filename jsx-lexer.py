dd27dec92467fa6fc431c99631f44b54650b808b
CPatchane <code@patchane.com>
2018-03-28 16:25:24 +0200

    add main root files :sparkles:

---
+++ jsx-lexer.py
@@ -0,0 +1,46 @@
+# source: https://github.com/fcurella/jsx-lexer
+
+import re
+
+from pygments.lexer import bygroups, include
+from pygments.lexers.javascript import JavascriptLexer
+from pygments.token import Name, Operator, Punctuation, String, Text
+
+
+# Use same tokens as `JavascriptLexer`, but with tags and attributes support
+TOKENS = JavascriptLexer.tokens
+TOKENS.update({
+    'jsx': [
+        (r'(<)([\w_\-]+)',
+         bygroups(Punctuation, Name.Tag), 'tag'),
+        (r'(<)(/)(\s*)([\w_\-]+)(\s*)(>)',
+         bygroups(Punctuation, Punctuation, Text, Name.Tag, Text,
+                  Punctuation)),
+    ],
+    'tag': [
+        (r'\s+', Text),
+        (r'([\w]+\s*)(=)(\s*)', bygroups(Name.Attribute, Operator, Text),
+         'attr'),
+        (r'[{}]+', Punctuation),
+        (r'[\w\.]+', Name.Attribute),
+        (r'(/?)(\s*)(>)', bygroups(Punctuation, Text, Punctuation), '#pop'),
+    ],
+    'attr': [
+        ('\s+', Text),
+        ('".*?"', String, '#pop'),
+        ("'.*?'", String, '#pop'),
+        (r'[^\s>]+', String, '#pop'),
+    ],
+})
+TOKENS['root'].insert(0, include('jsx'))
+
+
+class JsxLexer(JavascriptLexer):
+    name = 'react'
+    aliases = ['jsx', 'react']
+    filenames = ['*.jsx', '*.react']
+    mimetypes = ['text/jsx', 'text/typescript-jsx']
+
+    flags = re.MULTILINE | re.DOTALL | re.UNICODE
+
+    tokens = TOKENS
